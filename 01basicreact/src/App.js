f9eac0b778476bde3eb67db8d2d10b545f9e216a
PeeyushRathore <peeyushrathore2004@gmail.com>
2024-02-13 22:11:10 +0530

    installation

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Chai aur React | Peeyush Rathore</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+function App() {
+  
+
+  return (
+    <h1>Chai aur react with vite | Peeyush Rathore</h1>
+  )
+}
+
+export default App
