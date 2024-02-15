0b0dcd9e5ca19d0f30e9a71f50fbb9299c60111d
PeeyushRathore <peeyushrathore2004@gmail.com>
2024-02-15 15:33:41 +0530

    learn hook

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,11 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  let [counter, setCounter] = useState(15)
   
-  let counter = 15;
+  //let counter = 15;
   const addvalue = () => {
-    console.log("value added",Math.random());
+    //console.log("value added",Math.random());
     counter = counter+1;
+    console.log("clicked", counter)
+    setCounter(counter);
+  }
+  const removeValue = () =>{
+    setCounter(counter-1)
   }
 
   return (
@@ -18,7 +24,8 @@ function App() {
       <h2>Counter value: {counter}</h2>
       <button onClick={addvalue}>Add value</button>
       <br />
-      <button>Remove value</button>
+      <button onClick={removeValue}>Remove value {counter}</button>
+      <p>footer: {counter}</p>
     </>
   )
 }
